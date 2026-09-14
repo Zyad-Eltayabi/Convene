d925bc38396520ef51ac61e6b631c2a825582cbf
Zyad Eltayibi <ZyadEltayibi@gmail.com>
2026-09-15 01:33:49 +0300

    Add React Query and Devtools for data fetching; refactor App component to use useQuery

--- Frontend/src/app/layout/App.tsx
+++ Frontend/src/app/layout/App.tsx
@@ -1,78 +1,91 @@
 import { Box, Container, CssBaseline } from "@mui/material";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
   const [editMode, setEditMode] = useState(false);
 
-  useEffect(() => {
-    axios.get<Activity[]>('https://localhost:5001/api/activities')
-      .then(response => setActivities(response.data))
-  }, []);
+  const { data: activities, isPending } = useQuery({
+    queryKey: ["activities"],
+    queryFn: async () => {
+      const response = await axios.get<Activity[]>(
+        "https://localhost:8080/api/activities",
+      );
+      return response.data;
+    },
+  });
 
   const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
+    setSelectedActivity(activities!.find((x) => x.id === id));
+  };
 
   const handleCancelSelect = () => {
     setSelectedActivity(undefined);
-  }
+  };
 
   const handleOpenForm = (id?: string) => {
     if (id) handleSelectActivity(id);
     else handleCancelSelect();
     setEditMode(true);
-  }
+  };
 
   const handleFormClose = () => {
     setEditMode(false);
-  }
+  };
 
   const handleSubmitForm = async (activity: Activity) => {
-    if (activity.id) {
-      setActivities(activities.map(x => x.id === activity.id ? activity : x));
-      setSelectedActivity(activity)
-    } else {
-      const newActivity = { ...activity, id: activities.length.toString() }
-      setSelectedActivity(newActivity)
-      setActivities([...activities, newActivity]);
-    }
+    // if (activity.id) {
+    //   setActivities(
+    //     activities.map((x) => (x.id === activity.id ? activity : x)),
+    //   );
+    //   setSelectedActivity(activity);
+    // } else {
+    //   const newActivity = { ...activity, id: activities.length.toString() };
+    //   setSelectedActivity(newActivity);
+    //   setActivities([...activities, newActivity]);
+    // }
+    console.log(activity);
     setEditMode(false);
-  }
+  };
 
   const handleDeleteActivity = async (id: string) => {
-    try {
-      setActivities(activities.filter(x => x.id !== id))
-    } catch (error) {
-      console.log(error);
-    }
-  }
+    // try {
+    //   setActivities(activities.filter((x) => x.id !== id));
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    console.log(id);
+  };
 
   return (
-    <Box sx={{ bgcolor: '#eeeeee' }}>
+    <Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
       <CssBaseline />
       <NavBar openForm={handleOpenForm} />
-      <Container maxWidth='xl' sx={{ mt: 3 }}>
-        <ActivityDashboard
-          activities={activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelect={handleCancelSelect}
-          editMode={editMode}
-          openForm={handleOpenForm}
-          closeForm={handleFormClose}
-          submitForm={handleSubmitForm}
-          deleteActivity={handleDeleteActivity}
-        />
+      <Container maxWidth="xl" sx={{ mt: 3 }}>
+        {!activities || isPending ? (
+          <p>Loading...</p>
+        ) : (
+          <ActivityDashboard
+            activities={activities}
+            selectedActivity={selectedActivity}
+            selectActivity={handleSelectActivity}
+            cancelSelect={handleCancelSelect}
+            editMode={editMode}
+            openForm={handleOpenForm}
+            closeForm={handleFormClose}
+            submitForm={handleSubmitForm}
+            deleteActivity={handleDeleteActivity}
+          />
+        )}
       </Container>
-
     </Box>
-
-  )
+  );
 }
 
-export default App
+export default App;
