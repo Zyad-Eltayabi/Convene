f9e403fe6ec9c692e398b240c939f38d676b6e6d
Zyad Eltayibi <ZyadEltayibi@gmail.com>
2026-09-17 03:26:40 +0300

    Update ActivityForm to display dynamic title based on activity state; add loading indicator

--- Frontend/src/features/activities/form/ActivitityForm.tsx
+++ Frontend/src/features/activities/form/ActivitityForm.tsx
@@ -30,13 +30,15 @@ export default function ActivityForm() {
     }
   };
 
+  
+
   if (isActivityLoading) {
     return <Typography>Loading...</Typography>;
   }
   return (
     <Paper sx={{ borderRadius: 3, padding: 3 }}>
       <Typography variant="h5" gutterBottom color="primary">
-        Create activity
+        {activity ? "Edit Activity" : "Create Activity"}
       </Typography>
       <Box
         component="form"
