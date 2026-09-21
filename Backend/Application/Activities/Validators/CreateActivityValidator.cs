a502289f7ff733d7e7f8d7455fccb2ce6bb4d871
Zyad Eltayibi <ZyadEltayibi@gmail.com>
2026-09-21 08:34:24 +0300

    add validation for latitude and longitude in CreateActivityValidator
    - ensure Activity.Latitude is between -90 and 90
    - ensure Activity.Longitude is between -180 and 180

--- Backend/Application/Activities/Validators/CreateActivityValidator.cs
+++ Backend/Application/Activities/Validators/CreateActivityValidator.cs
@@ -30,6 +30,12 @@ public class CreateActivityValidator : AbstractValidator<CreateActivity.Command>
         RuleFor(x => x.Activity.Venue)
             .NotEmpty().WithMessage("Venue is required.")
             .Length(2, 200).WithMessage("Venue must be between 2 and 200 characters.");
+
+        RuleFor(x => x.Activity.Latitude)
+            .InclusiveBetween(-90, 90).WithMessage("Latitude must be between -90 and 90.");
+
+        RuleFor(x => x.Activity.Longitude)
+            .InclusiveBetween(-180, 180).WithMessage("Longitude must be between -180 and 180.");
     }
 
 }
